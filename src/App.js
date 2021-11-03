1c8ef0e548ae135855d492db26a08e8ccffeca95
ivioje <rhodaivioje@gmail.com>
2021-11-03 16:55:36 +0100

    added home page files

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
+import HomePage from './HomePage';
 
 function App() {
   return (
     <div className="App">
-     hello
+     <HomePage />
     </div>
   );
 }
