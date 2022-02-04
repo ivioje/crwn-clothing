375a1cdf506616098bd113c8ad97ba795aa53a61
ivioje <rhodaivioje@gmail.com>
2022-02-04 18:50:19 +0100

    added redux and the user reducer

--- src/index.js
+++ src/index.js
@@ -3,13 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Provider>
     <Router>
-    <App />
+      <App />
     </Router>
-  </React.StrictMode>,
+  </Provider>,
   document.getElementById('root')
 );
 
---
+++ src/redux/root-reducer.js
@@ -0,0 +1,8 @@
+//base reducer
+import { combineReducers } from "redux";
+
+import userReducer from "./user/user.reducer";
+
+export default combineReducers({
+    user: userReducer
+})
